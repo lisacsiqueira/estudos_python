d6ec5de161ae020394e9eb5571a61db31e5aeb87
Lisa Costa <112580152+lisacsiqueira@users.noreply.github.com>
2022-10-03 15:22:53 -0300

    Novo código

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Estudos Python
 
 Resolução de questões para o estudo de Python.
+Os níveis das questões estão variados
---
+++ quest02.py
@@ -0,0 +1,11 @@
+"""
+Leia 2 valores inteiros e armazene-os nas variáveis A e B. 
+Efetue a soma de A e B atribuindo o seu resultado na variável X. 
+Imprima X
+"""
+a = int(input())
+b = int(input())
+
+x = a + b
+
+print("X = {}".format(x))
